a324277c765e61b06cc0f7582cb09cc1a27144fb
Raman Kumar <40249679+codemunic@users.noreply.github.com>
2019-08-19 23:11:04 +0530

    Update dkrl.py

--- ndkgc/models/dkrl.py
+++ ndkgc/models/dkrl.py
@@ -9,8 +9,13 @@ import tensorflow.contrib.training as training
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+print('Before importing get_lookup_table')
+
 from ndkgc.ops import get_lookup_table, corrupt_single_relationship, corrupt_single_entity, content_lookup, \
     multiple_content_lookup, normalized_lookup, avg_grads
+
+print('Before utils file')
+
 from ndkgc.utils import count_line, valid_vocab_file, load_list, \
     load_triples, load_pretrained_embedding, load_content
 
