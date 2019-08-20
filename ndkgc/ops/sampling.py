914209dd79def08cfc0cf2f5042869fecc159ac2
Raman Kumar <40249679+codemunic@users.noreply.github.com>
2019-08-20 22:04:41 +0530

    Update sampling.py

--- ndkgc/ops/sampling.py
+++ ndkgc/ops/sampling.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import os
 
 __so_dir = os.path.dirname(os.path.abspath(__file__))
+print(_so_dir)
 __lib = tf.load_op_library(os.path.join(__so_dir, './__sampling/libsampling.so'))
 
 single_negative_sampling = __lib.single_negative_sampling
