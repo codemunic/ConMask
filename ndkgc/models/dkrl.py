1ce910aa09c08d14b4a139151f9f5db105b7817f
Raman Kumar <40249679+codemunic@users.noreply.github.com>
2019-08-26 18:18:38 +0530

    Update dkrl.py

--- ndkgc/models/dkrl.py
+++ ndkgc/models/dkrl.py
@@ -928,7 +928,7 @@ def main(_):
                     cnt += 1
                     if cnt % 10 == 0:
                         _, loss, global_step = sess.run([train_op, loss_op, model.global_step])
-                        #print("GSTEP:_%d_LOSS:_%.4f" % (global_step, loss), end='\r')
+                        print("GSTEP:_%d_LOSS:_%.4f" % (global_step, loss), end='\r')
                     else:
                         sess.run(train_op)
             except tf.errors.OutOfRangeError:
