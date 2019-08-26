7fec30e65ef74006276a50b9f318573c4bf4fe21
Raman Kumar <40249679+codemunic@users.noreply.github.com>
2019-08-26 11:51:11 +0530

    Update dkrl.py

--- ndkgc/models/dkrl.py
+++ ndkgc/models/dkrl.py
@@ -393,9 +393,9 @@ class DKRL(object):
                 
                 single_triple = tf.train.shuffle_batch([input_triples],
                                                        batch_size=1,
-                                                       capacity=min(batch_size * 40, float(self.train_matrix.get_shape()[0])),
+                                                       capacity=min(batch_size * 40, self.train_matrix.get_shape().as_list()[0]),
                                                        min_after_dequeue=min(batch_size * 15,
-                                                                             float(self.train_matrix.get_shape()[0])),
+                                                                             self.train_matrix.get_shape().as_list()[0]),
                                                        enqueue_many=True,
                                                        allow_smaller_final_batch=True,
                                                        name='train_shuffle_batch')
